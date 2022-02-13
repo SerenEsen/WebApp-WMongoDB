e4305fb18d8613145f93d4ade575c7760a2bc987
SerenEsen <74063743+SerenEsen@users.noreply.github.com>
2022-02-13 22:20:30 +0300

    site.cs

---
+++ site.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YazılımSitesi.Controllers
+{
+    public class site
+    {
+        public int Id { get; set; }
+        public string site_isim { get; set; }
+    }
+}
